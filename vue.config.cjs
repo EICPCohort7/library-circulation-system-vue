db5392f89661038f508c0721e681cdad395cad20
John Paxton <johnpaxton@gmail.com>
2022-11-15 02:05:52 -0500

    Webpack config

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+  outputDir: 'server/public',
   transpileDependencies: true,
   configureWebpack: {
     entry: './front-end/src/main.js',
